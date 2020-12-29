deacdd453d2bad18618501dd235613ed30f1f79a
odajs <60214927+odajs@users.noreply.github.com>
2020-12-29 12:26:50 +0300

    Update app.js

--- odajs/app.js
+++ odajs/app.js
@@ -1,14 +1,18 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    databases: []
-  }
-})
-
-function loadSamples() {
-  app.databases = ENV.generateData().toArray();
-  Monitoring.renderRate.ping();
-  setTimeout(loadSamples, ENV.timeout);
-}
-
-loadSamples()
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+    <head>
+        <meta name="description" content="dbmon oda" />
+        <link href="../styles.css" rel="stylesheet" type="text/css" />
+        <title>dbmon (ODA)</title>
+    </head>
+    <body>
+        <script src="../ENV.js"></script>
+        <script src="../lib/memory-stats.js"></script>
+        <script src="../lib/monitor.js"></script>
+        <!-- <script src="../lib/oda.js"></script> -->
+        <script type="module" src="./app.js"></script>
+        <script src="../ga.js"></script>
+        <oda-pref></oda-pref>
+    </body>
+</html>
